59260914dd08643aab42c7e8ca1f0e34a67a3c1d
muqcit <muqcit@gmail.com>
2024-10-30 03:44:30 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // Configure CORS to allow requests only from your frontend
 app.use(cors()); // Replace with your frontend's origin
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
