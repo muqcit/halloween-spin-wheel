ff7477452828468fc49e9370b39268457cb58fb0
ShadowFighterm <130086707+ShadowFighterm@users.noreply.github.com>
2024-10-30 02:09:27 +0500

    Fixes

--- src/components/spinWheel.jsx
+++ src/components/spinWheel.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styles from "./SpinWheel.module.css";
+import styles from "./spinWheel.module.css";
 import { useLocation } from "react-router-dom";
 
 const pies = [
